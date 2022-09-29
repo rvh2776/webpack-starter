cb32cb078181a7ab0c59026137d786e9cb0624c2
Rafael V.H <rafael.vh@gmail.com>
2022-09-29 18:19:27 -0300

    Resubo WebPack

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
+import { saludar } from './js/componentes';
 import './styles.css';
 
-import {Todo, TodoList } from './classes';
-import { crearTodoHtml } from './js/componentes';
+const nombre = 'Rafael';
 
-
-export const todoList = new TodoList();
-
-// todoList.todos.forEach(todo => crearTodoHtml(todo));
-
-todoList.todos.forEach(crearTodoHtml); // es lo mismo que la linea anterior 
-                                       //pero como se envia un solo argumento no es necesario ponerlo.
-
-
-   //console.log('todos', todoList.todos);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const tarea = new Todo('Aprender Node.');
-// todoList.nuevoTodo(tarea);
-
-// console.log(todoList);
-
-// crearTodoHtml(tarea);
-
-// localStorage.setItem('clave', '123456');
-// sessionStorage.setItem('clave', '1234');
-
-// setTimeout(() => {
-//     localStorage.removeItem('clave');
-// }, 1500);
-
+saludar( nombre );
